994e54e33040ec076433f94f80512c731a0dfc7d
paolopedroso <108847100+paolopedroso@users.noreply.github.com>
2025-03-31 14:44:23 -0700

    file add

---
+++ rtl/core/salu.sv
@@ -0,0 +1,19 @@
+    /*
+     * Scalar ALU
+     *
+     * @copyright 2025 Paolo Pedroso <paoloapedroso@gmail.com>
+     *
+     * @license Apache 2.0
+     *
+     */
+
+module salu #(
+    parameter int DATA_WIDTH = 32
+) (
+    input logic clk,
+    input logic rst_n
+);
+
+
+
+endmodule
