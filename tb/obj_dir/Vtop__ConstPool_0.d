178881f7bbab869342535ffbced0aaf616c10eb4
Paolo Pedroso <paoloapedroso@gmail.com>
2025-05-20 22:54:55 -0700

    fix

--- tb/obj_dir/Vtop__ConstPool_0.d
+++
@@ -1,2 +0,0 @@
-Vtop__ConstPool_0.o: Vtop__ConstPool_0.cpp \
- /usr/share/verilator/include/verilated.h
--- tb/obj_dir/Vtop__pch.h.slow.d
+++
@@ -1,8 +0,0 @@
-Vtop__pch.h.slow.gch: Vtop__pch.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h Vtop__Syms.h Vtop.h \
- Vtop___024root.h /usr/share/verilator/include/verilated_timing.h \
- /usr/share/verilator/include/verilated.h
