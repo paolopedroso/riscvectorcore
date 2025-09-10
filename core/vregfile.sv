190336c33d6f6e9816adcc2f28004565c6af8eb5
Paolo Pedroso <108847100+paolopedroso@users.noreply.github.com>
2025-09-10 12:13:52 -0700

    updated sync logic

--- core/vregfile.sv
+++ core/vregfile.sv
@@ -25,7 +25,7 @@ module vregfile #(
 
 );
 
-// 8196 bit memory
+// 8192 bit memory
 // 1024 bit VLEN
 logic [DATA_WIDTH-1:0] vregs [VREGS][ELEMENTS];
 
@@ -36,17 +36,7 @@ always_ff @(posedge clk) begin
                 vregs[i][j] <= '0;
             end
         end
-    end
-
-    if (vregw_en_i && (vrd_addr_i != 0)) begin
-        // Optimize
-        // for (int i=0; i<ELEMENTS/4; i+=4) begin
-        //     vregs[vrd_addr_i][i  ] <= vrd_data_i[((i  )*DATA_WIDTH) +: DATA_WIDTH];
-        //     vregs[vrd_addr_i][i+1] <= vrd_data_i[((i+1)*DATA_WIDTH) +: DATA_WIDTH];
-        //     vregs[vrd_addr_i][i+2] <= vrd_data_i[((i+2)*DATA_WIDTH) +: DATA_WIDTH];
-        //     vregs[vrd_addr_i][i+3] <= vrd_data_i[((i+3)*DATA_WIDTH) +: DATA_WIDTH];
-        // end
-
+    end else if (vregw_en_i && (vrd_addr_i != 0)) begin
         for (int i=0; i<ELEMENTS; i++) begin
             vregs[vrd_addr_i][i] <= vrd_data_i[((i)*DATA_WIDTH) +: DATA_WIDTH];
         end
