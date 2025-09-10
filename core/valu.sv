d1ba0956f55372404cd1242cfe3f8538389c89a7
Paolo Pedroso <108847100+paolopedroso@users.noreply.github.com>
2025-09-10 12:13:21 -0700

    updated valu.sv

--- core/valu.sv
+++ core/valu.sv
@@ -16,33 +16,105 @@ module valu #(
     input logic [DATA_WIDTH-1:0] vrs1_data_in [ELEMENTS],
     output logic [DATA_WIDTH-1:0] valu_res_o [ELEMENTS]
 );
-// [8][8][8][8][8][8][8]
-//          +
-// [8][8][8][8][8][8][8]
 
 localparam logic [3:0] 
-    VADD   = 4'b0000;
-    // VSUB   = 4'b0001,
-    // VSLL   = 4'b0010,
-    // VSLT   = 4'b0011,
-    // VSLTU  = 4'b0100,
-    // VXOR   = 4'b0101,
-    // VSRL   = 4'b0110,
-    // VSRA   = 4'b0111,
-    // VOR    = 4'b1000,
-    // VAND   = 4'b1001;
-    // BEQ   = 4'b1010,
-    // BNE   = 4'b1011,
-    // BLT   = 4'b1100,
-    // BGE   = 4'b1101,
-    // BLTU  = 4'b1110,
-    // BGEU  = 4'b1111;
+        VADD   = 4'b0000,
+        VSUB   = 4'b0001,
+        VSLL   = 4'b0010,
+        VSLT   = 4'b0011,
+        VSLTU  = 4'b0100,
+        VXOR   = 4'b0101,
+        VSRL   = 4'b0110,
+        VSRA   = 4'b0111,
+        VOR    = 4'b1000,
+        VAND   = 4'b1001,
+        BEQ    = 4'b1010,
+        BNE    = 4'b1011,
+        BLT    = 4'b1100,
+        BGE    = 4'b1101,
+        BLTU   = 4'b1110,
+        BGEU   = 4'b1111;
 
 always_comb begin
     case(valu_op_in)
         VADD: begin
             for (int i=0; i<ELEMENTS; i++) begin
                 valu_res_o[i] = vrs1_data_in[i] + vrs2_data_in[i];
+            end
+        end
+        VSUB: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = vrs1_data_in[i] - vrs2_data_in[i];
+            end
+        end
+        VSLL: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = vrs1_data_in[i] << vrs2_data_in[i][4:0];
+            end
+        end
+        VSLT: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = ($signed(vrs1_data_in[i]) < $signed(vrs2_data_in[i])) ? 32'd1 : 32'd0;
+            end
+        end
+        VSLTU: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = (vrs1_data_in[i] < vrs2_data_in[i]) ? 32'd1 : 32'd0;
+            end
+        end
+        VXOR: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = vrs1_data_in[i] ^ vrs2_data_in[i];
+            end
+        end
+        VSRL: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = vrs1_data_in[i] >> vrs2_data_in[i][4:0];
+            end
+        end
+        VSRA: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = $signed(vrs1_data_in[i]) >>> vrs2_data_in[i][4:0];
+            end
+        end
+        VOR: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = vrs1_data_in[i] | vrs2_data_in[i];
+            end
+        end
+        VAND: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = vrs1_data_in[i] & vrs2_data_in[i];
+            end
+        end
+        BEQ: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = (vrs1_data_in[i] == vrs2_data_in[i]) ? 32'd1 : 32'd0;
+            end
+        end
+        BNE: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = (vrs1_data_in[i] != vrs2_data_in[i]) ? 32'd1 : 32'd0;
+            end
+        end
+        BLT: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = ($signed(vrs1_data_in[i]) < $signed(vrs2_data_in[i])) ? 32'd1 : 32'd0;
+            end
+        end
+        BGE: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = ($signed(vrs1_data_in[i]) >= $signed(vrs2_data_in[i])) ? 32'd1 : 32'd0;
+            end
+        end
+        BLTU: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = (vrs1_data_in[i] < vrs2_data_in[i]) ? 32'd1 : 32'd0;
+            end
+        end
+        BGEU: begin
+            for (int i=0; i<ELEMENTS; i++) begin
+                valu_res_o[i] = (vrs1_data_in[i] >= vrs2_data_in[i]) ? 32'd1 : 32'd0;
             end
         end
         default: begin
